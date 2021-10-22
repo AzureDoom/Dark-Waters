3aa1a968bb797c73d35e8892395d55ca9cf58fff
AzureZhen <rcraft@cyberconnects2.com>
2021-10-22 01:29:53 -0400

    Rendering updated

--- src/main/java/mod/azure/darkwaters/client/renders/ManarawRender.java
+++ src/main/java/mod/azure/darkwaters/client/renders/ManarawRender.java
@@ -8,7 +8,6 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
-import software.bernie.geckolib3.geo.render.built.GeoModel;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
 public class ManarawRender extends GeoEntityRenderer<ManarawEntity> {
@@ -22,13 +21,5 @@ public class ManarawRender extends GeoEntityRenderer<ManarawEntity> {
 			VertexConsumerProvider renderTypeBuffer, VertexConsumer vertexBuilder, int packedLightIn,
 			Identifier textureLocation) {
 		return RenderLayer.getEntityTranslucent(getTextureLocation(animatable));
-	}
-
-	@Override
-	public void render(GeoModel model, ManarawEntity animatable, float partialTicks, RenderLayer type,
-			MatrixStack matrixStackIn, VertexConsumerProvider renderTypeBuffer, VertexConsumer vertexBuilder,
-			int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
-		super.render(model, animatable, partialTicks, type, matrixStackIn, renderTypeBuffer, vertexBuilder,
-				packedLightIn, packedOverlayIn, red, green, blue, 0.7F);
 	}
 }
--- src/main/java/mod/azure/darkwaters/client/renders/MohastRender.java
+++ src/main/java/mod/azure/darkwaters/client/renders/MohastRender.java
@@ -8,7 +8,6 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
-import software.bernie.geckolib3.geo.render.built.GeoModel;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
 public class MohastRender extends GeoEntityRenderer<MohastEntity> {
@@ -22,13 +21,5 @@ public class MohastRender extends GeoEntityRenderer<MohastEntity> {
 			VertexConsumerProvider renderTypeBuffer, VertexConsumer vertexBuilder, int packedLightIn,
 			Identifier textureLocation) {
 		return RenderLayer.getEntityTranslucent(getTextureLocation(animatable));
-	}
-
-	@Override
-	public void render(GeoModel model, MohastEntity animatable, float partialTicks, RenderLayer type,
-			MatrixStack matrixStackIn, VertexConsumerProvider renderTypeBuffer, VertexConsumer vertexBuilder,
-			int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
-		super.render(model, animatable, partialTicks, type, matrixStackIn, renderTypeBuffer, vertexBuilder,
-				packedLightIn, packedOverlayIn, red, green, blue, 0.7F);
 	}
 }
